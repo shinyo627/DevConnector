064642136491a6341579a3e2ba024c3578c40eb3
shinyo627 <shinyo627@gmail.com>
2021-01-29 18:24:26 +0100

    Removed some unnecessarily cached files

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
 // Connect Database
 connectDB();
-
-app.get('/', (req, res) => {
-  res.send('API running ');
-});
 
 // Initialize Middlewares
 // express.json() middle ware is for parsing body data. Basically we can use req.body on our call
@@ -20,7 +17,17 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-// If process.env.PORT will come in when deployed to heroku otherwise we use  5000
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+// process.env.PORT will come in when deployed to heroku otherwise we use  5000
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
